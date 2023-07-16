54648a578cf6a45721f4c34079f608bc6974611d
Pranav K <pranavkcse@gmail.com>
2023-07-16 21:08:51 +0530

    Part 7 Completed

--- Basics/part7.js
+++ Basics/part7.js
@@ -7,24 +7,46 @@
 // Node.js Routing and GET Request without Express
 
 var http = require("http");
-var fs = require('fs')
+var fs = require("fs");
+var url = require("url")
 
-http.createServer(function (req,res) {
+http
+  .createServer(function (req, res) {
 
-    if (req.url=='/') {
-        
-    }else if(req.url=='/login'){
+    var q = url.parse(req.url,true);
+    console.log(q.pathname);
 
-    }else{
-        
-    }
+    if (q.pathname == "/") {
+    
+      fs.readFile("sample.html", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+      });
+      
+    } else if (q.pathname == "/signup") {
+      
+      fs.readFile("signup.html", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+      });
+    
+    } else if (q.pathname == '/signupaction') {
 
-
-    fs.readFile('sample.html', function(err,data){
-      res.writeHead(200,{'Content-Type':'text/html'})
-      res.write(data);
+      console.log(q.query.lname);
+      
+      res.write('<h1>'+q.query.fname+'</h1>');
       res.end();
-    })
+      
+    } else {
+      
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.write("404 error !!");
+      res.end();
+    
+    }
   })
-  .listen(7000);
-
+  .listen(7000,() => {
+    console.log("Server Started\n@localhost : localhost:7000");
+  });
